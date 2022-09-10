5ed3915d414259645c458337beccca669158deb0
Sateesh Kumar <sateeshjumbo@gmail.com>
2022-09-10 14:55:14 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ebook_logs
+
+Keep track of your reading habits and discover new material
+
+## App Showcase
---
+++ android/app/src/main/kotlin/com/sateeshjh/ebook_logs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sateeshjh.ebook_logs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
